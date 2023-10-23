f385844a412bbc6bc95cf51d22b600d61c92c7ac
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2023-10-23 15:03:11 +0700

    second commit

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerBtn = document.getElementById('hamburger-btn');
-const rightNav = document.querySelector('.right');
-
-hamburgerBtn.addEventListener('click', () => {
-    rightNav.classList.toggle('hidden');
-});
